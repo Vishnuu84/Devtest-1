7c3a33f567bd452b6209b30f16f18b4b9ad31dd7
Chinthala Vishnuu <chinthalavishnuu@gmail.com>
2025-07-24 10:19:35 +0530

    inter level code.java

---
+++ Inter level Code.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Main
+{
+ 	public static void main(String[] args)
+    {
+		Scanner sc=new Scanner(System.in);
+		int number1=sc.nextInt();
+		int number2=sc.nextInt();
+ 		int count=0;
+        if(number1< number2)
+	{
+		System.out.println("Not possible");
+	}
+	else 
+	{
+		boolean flag=false;
+		while(number1!=0 && number2!=0)
+		{
+			int temp=0;
+			int temp2=number2%10;
+			if(flag)
+				temp1=number1%10-1;
+			else 
+				temp1=number1%10;
+			if(temp1< temp2)
+			{
+				count++;
+				flag=true;
+			}
+			else{
+				flag=false;
+				number1=number1/10;
+				number2=number2/10;
+			}
+		}
+		System.out.println(count);
+	}
+    }
+}
