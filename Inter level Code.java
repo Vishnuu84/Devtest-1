3c18bd22cafed876933093207e53af03b05843f7
Chinthala Vishnuu <chinthalavishnuu@gmail.com>
2025-07-24 14:24:43 +0530

    Update Inter level Code.java

--- Inter level Code.java
+++ Inter level Code.java
@@ -1,39 +1,26 @@
 import java.util.*;
 public class Main
 {
- 	public static void main(String[] args)
+    public static void moveHash(String str ,int n)
     {
-		Scanner sc=new Scanner(System.in);
-		int number1=sc.nextInt();
-		int number2=sc.nextInt();
- 		int count=0;
-        if(number1< number2)
-	{
-		System.out.println("Not possible");
-	}
-	else 
-	{
-		boolean flag=false;
-		while(number1!=0 && number2!=0)
-		{
-			int temp=1;
-			int temp2=number2%10;
-			if(flag)
-				temp1=number1%10-1;
-			else 
-				temp1=number1%10;
-			if(temp1< temp2)
-			{
-				count++;
-				flag=true;
-			}
-			else{
-				flag=false;
-				number1=number1/10;
-				number2=number2/10;
-			}
-		}
-		System.out.println(count);
-	}
+        String str1= new String("");
+        String str2= new String("");
+        int i=0;
+        for(i=0;i < n;i++)
+        {
+            if(str.charAt(i)=='#')
+                str1=str1 + str.charAt(i);
+            else
+                str2 = str2 + str.charAt(i);
+        }
+        String result = str1.concat(str2);
+        System.out.println(result);
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        int len= a.length();
+        moveHash(a, len);
     }
 }
